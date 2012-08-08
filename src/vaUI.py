d082e7b43cdf6812b15c89cc41545f7a9ffbd056
Yaw Anokwa <yanokwa@nafundi.com>
2012-08-08 11:16:38 -0700

    Most buttons now do something

--- src/vaUI.py
+++ src/vaUI.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 
+# TODO: help should pop up window
+# TODO: bug in radio grouping
+# TODO: pull out all strings
+# TODO: why is the first button selected
+
+import os
 import wx
 
 APP_EXIT = 1
+APP_HELP = 2
 
 class vaUI(wx.Frame):
     
     def __init__(self, parent, title):
         super(vaUI, self).__init__(parent, title=title, 
-            size=(550, 750),style=wx.CAPTION)
+            size=(550, 760),style=wx.CAPTION)
             
         self.InitUI()
         self.Centre()
@@ -16,120 +23,214 @@ class vaUI(wx.Frame):
         
     def InitUI(self):
 
+        self.inputFilePath = ""
+        self.outputFolderPath = ""
+        self.statusLog = ""
+
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
+        helpMenu = wx.Menu()
         qmi = wx.MenuItem(fileMenu, APP_EXIT, '&Quit\tCtrl+Q')
+        hmi = wx.MenuItem(helpMenu, APP_HELP, '&Help')
         fileMenu.AppendItem(qmi)
+        helpMenu.AppendItem(hmi)
         menubar.Append(fileMenu, '&File')
+        menubar.Append(helpMenu, '&Help')
         
-        self.Bind(wx.EVT_MENU, self.OnQuit, id=APP_EXIT)
+        self.Bind(wx.EVT_MENU, self.onQuit, id=APP_EXIT)
         self.SetMenuBar(menubar)
 
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
-        r = wx.BoxSizer(wx.HORIZONTAL)
         r0 = wx.BoxSizer(wx.HORIZONTAL)
+        r1 = wx.BoxSizer(wx.HORIZONTAL)
 
-
-       
-        # actually you can load .jpg  .png  .bmp  or .gif files
         scaleSize = .35
         imageFile = '../res/logo.png'
         image = wx.Image(imageFile, wx.BITMAP_TYPE_ANY)
         scaled_image = image.Scale(image.GetWidth()*scaleSize, image.GetHeight()*scaleSize, wx.IMAGE_QUALITY_HIGH).ConvertToBitmap()
-        r.AddStretchSpacer()
-        r.Add(wx.StaticBitmap(panel,-1,scaled_image),flag=wx.RIGHT|wx.BOTTOM, border=12)
-        r.AddStretchSpacer()
+        r0.AddStretchSpacer()
+        r0.Add(wx.StaticBitmap(panel,-1,scaled_image),flag=wx.RIGHT, border=12)
+        r0.AddStretchSpacer()
 
 
-        # r0.AddStretchSpacer()
-        # r0.Add(wx.Button(panel, label="Help"), flag=wx.RIGHT, border=12)
-        # r0.Add(wx.Button(panel, label="Quit"), flag=wx.RIGHT, border=12)
-        # r0.AddStretchSpacer()
-
-        r1 = wx.BoxSizer(wx.HORIZONTAL)
-
-        r1sb1 = wx.StaticBox(panel, label="1. Input file")
-        r1sbs1 = wx.StaticBoxSizer(r1sb1, wx.HORIZONTAL)
-        r1sbs1.Add(wx.Button(panel, label="Choose file..."), flag=wx.ALL, border=5)
-        r1sbs1.Add(wx.StaticText(panel, label="/Hard Drive/User/Documents/input.csv",size=(367, -1)), proportion=1, flag=wx.ALL, border=5)
+        r1.AddStretchSpacer()
+        r1.Add(wx.Button(panel, label="Help"), flag=wx.RIGHT, border=12)
         
-        r1.Add(r1sbs1)
+        r2 = wx.BoxSizer(wx.HORIZONTAL)
 
+        r2sb1 = wx.StaticBox(panel, label="1. Input file")
+        r2sbs1 = wx.StaticBoxSizer(r2sb1, wx.HORIZONTAL)
 
-        r2 = wx.BoxSizer(wx.HORIZONTAL)        
+        chooseFileButton = wx.Button(panel, label="Choose file...")
+        chooseFileButton.Bind(wx.EVT_BUTTON, self.onOpenFile)
+        r2sbs1.Add(chooseFileButton, flag=wx.ALL, border=5)
 
-        r2sb1 = wx.StaticBox(panel, label="2. Input type")
-        r2sbs1 = wx.StaticBoxSizer(r2sb1, wx.VERTICAL)
-        r2sbs1.Add(wx.RadioButton(panel, label="Adult"), flag=wx.LEFT|wx.TOP, border=5)
-        r2sbs1.Add(wx.RadioButton(panel, label="Child"), flag=wx.LEFT|wx.TOP, border=5)
-        r2sbs1.Add(wx.RadioButton(panel, label="Neonatal"), flag=wx.LEFT|wx.TOP, border=5)
-        r2sbs1.AddSpacer(10) 
-        r2sbs1.Add(wx.CheckBox(panel, label="HCE variables"), flag=wx.LEFT|wx.TOP, border=5)
-        r2sbs1.AddSpacer(3)
-
-        r2sb2 = wx.StaticBox(panel, label="3. Algorithm type",)
-        r2sbs2 = wx.StaticBoxSizer(r2sb2, wx.VERTICAL)
-        r2sbs2.Add(wx.RadioButton(panel, label="Tariff"), flag=wx.LEFT|wx.TOP, border=5)
-        r2sbs2.Add(wx.RadioButton(panel, label="Random forest"), flag=wx.LEFT|wx.TOP, border=5)
-        r2sbs2.AddSpacer(4)
-
-        r2.Add(r2sbs1,proportion=1,flag=wx.RIGHT, border=10)
-        r2.Add(r2sbs2,proportion=1,flag=wx.RIGHT, border=10)
-
-
-        r3 = wx.BoxSizer(wx.HORIZONTAL)
-
-        r3sb1 = wx.StaticBox(panel, label="4. Output folder")
-        r3sbs1 = wx.StaticBoxSizer(r3sb1, wx.HORIZONTAL)
-        r3sbs1.Add(wx.Button(panel, label="Choose folder..."), flag=wx.ALL, border=5)
-        r3sbs1.Add(wx.StaticText(panel, label="/Hard Drive/User/Documents",size=(348, -1)), proportion=1, flag=wx.EXPAND|wx.ALL, border=5)
+        self.choosenFileText = wx.StaticText(panel, label="",size=(367, -1))
+        r2sbs1.Add(self.choosenFileText, proportion=1, flag=wx.ALL, border=5)
         
-        r3.Add(r3sbs1)
+        r2.Add(r2sbs1)
 
 
-        r4 = wx.BoxSizer(wx.HORIZONTAL)
+        r4 = wx.BoxSizer(wx.HORIZONTAL)        
 
-        r4sb1 = wx.StaticBox(panel, label="5. Analysis status")
+        r4sb1 = wx.StaticBox(panel, label="2. Input type")
         r4sbs1 = wx.StaticBoxSizer(r4sb1, wx.VERTICAL)
-        r4e1 = wx.TextCtrl(panel,size=(475, 150),style=wx.TE_MULTILINE|wx.TE_CENTER)
-        r4e1.SetEditable(False)
-        r4e1.SetValue("Data validation complete.\n\nProcessing row 1...\nProcessing row 2...")
-        r4sbs1.Add(r4e1,flag=wx.ALL,border=5)
-        r4sbs1.AddSpacer(5)
-        r4e2 = wx.Gauge(panel,range=100,size=(471, -1))
-        r4e2.SetValue(40)
-        r4sbs1.Add(r4e2,flag=wx.RIGHT|wx.LEFT, border=7)
-        r4sbs1.AddSpacer(10)
-        r4sbs1.Add(wx.Button(panel, label="Stop"),flag=wx.RIGHT|wx.LEFT|wx.BOTTOM, border=5)
 
-        r4.Add(r4sbs1)
+        self.adultRadioButton = wx.RadioButton(panel, label="Adult", style=wx.RB_GROUP)
+        self.adultRadioButton.SetValue(True)
+        self.childRadioButton = wx.RadioButton(panel, label="Child")
+        self.neonatalRadioButton = wx.RadioButton(panel, label="Neonatal")
+        self.hceCheckBox = wx.CheckBox(panel, label="HCE variables")
+        self.hceCheckBox.SetValue(True)
+
+        r4sbs1.Add(self.adultRadioButton, flag=wx.LEFT|wx.TOP, border=5)
+        r4sbs1.Add(self.childRadioButton, flag=wx.LEFT|wx.TOP, border=5)
+        r4sbs1.Add(self.neonatalRadioButton, flag=wx.LEFT|wx.TOP, border=5)
+        r4sbs1.AddSpacer(10) 
+        r4sbs1.Add(self.hceCheckBox, flag=wx.LEFT|wx.TOP, border=5)
+        r4sbs1.AddSpacer(3)
+
+        r4sb2 = wx.StaticBox(panel, label="3. Algorithm type",)
+        r4sbs2 = wx.StaticBoxSizer(r4sb2, wx.VERTICAL)
+
+        self.randomForestRadioButton = wx.RadioButton(panel, label="Random forest", style=wx.RB_GROUP)
+        self.randomForestRadioButton.SetValue(True)
+        self.tariffRadioButton = wx.RadioButton(panel, label="Tariff")
+
+        r4sbs2.Add(self.randomForestRadioButton, flag=wx.LEFT|wx.TOP, border=5)
+        r4sbs2.Add(self.tariffRadioButton, flag=wx.LEFT|wx.TOP, border=5)
+        r4sbs2.AddSpacer(4)
+
+        r4.Add(r4sbs1,proportion=1,flag=wx.RIGHT, border=10)
+        r4.Add(r4sbs2,proportion=1,flag=wx.RIGHT, border=10)
 
 
-#        r5 = wx.StaticText(panel, label="http://www.healthmetricsandevaluation.org", style=wx.ALIGN_RIGHT)
-#        r5.SetFont(wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL))
-#        hbox.Add(r5)
+        r5 = wx.BoxSizer(wx.HORIZONTAL)
 
-        vbox.Add(r,flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        # vbox.Add(r0,flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+        r5sb1 = wx.StaticBox(panel, label="4. Output folder")
+        r5sbs1 = wx.StaticBoxSizer(r5sb1, wx.HORIZONTAL)
 
-        vbox.Add(r1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        vbox.AddSpacer(10)
+        chooseFolderButton = wx.Button(panel, label="Choose folder...")
+        chooseFolderButton.Bind(wx.EVT_BUTTON, self.onOpenFolder)
+        r5sbs1.Add(chooseFolderButton, flag=wx.ALL, border=5)
+
+        self.choosenFolderText = wx.StaticText(panel, label="",size=(367, -1))
+        r5sbs1.Add(self.choosenFolderText, proportion=1, flag=wx.EXPAND|wx.ALL, border=5)
+        
+        r5.Add(r5sbs1)
+
+
+        r6 = wx.BoxSizer(wx.HORIZONTAL)
+
+        r6sb1 = wx.StaticBox(panel, label="5. Analysis status")
+        r6sbs1 = wx.StaticBoxSizer(r6sb1, wx.VERTICAL)
+        
+        self.statusTextCtrl = wx.TextCtrl(panel,size=(475, 150),style=wx.TE_MULTILINE|wx.TE_CENTER)
+        self.statusTextCtrl.SetEditable(False)
+        self.statusTextCtrl.SetValue(self.statusLog)
+
+        r6sbs1.Add(self.statusTextCtrl,flag=wx.ALL,border=5)
+        r6sbs1.AddSpacer(5)
+
+        self.statusGauge = wx.Gauge(panel,range=100,size=(471, -1))
+        r6sbs1.Add(self.statusGauge,flag=wx.RIGHT|wx.LEFT, border=7)
+        r6sbs1.AddSpacer(10)
+
+        self.actionButton = wx.Button(panel, label="Start")
+        self.actionButton.Bind(wx.EVT_BUTTON, self.onAction)
+
+
+        r6sbs1.Add(self.actionButton,flag=wx.RIGHT|wx.LEFT|wx.BOTTOM, border=5)
+
+        r6.Add(r6sbs1)
+
+        vbox.Add(r0,flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+        vbox.Add(r1,flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+
         vbox.Add(r2, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
         vbox.AddSpacer(10)
-        vbox.Add(r3, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        vbox.AddSpacer(10)
         vbox.Add(r4, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-
-        # add link to ihme
-        # add imhe logo
-        # add help
+        vbox.AddSpacer(10)
+        vbox.Add(r5, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+        vbox.AddSpacer(10)
+        vbox.Add(r6, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
 
         panel.SetSizer(vbox)
 
+    def shortenPath(self,path, maxLength):
+        pathLength = 0
+        pathLengthMax = maxLength
+        shortenedPathList = []
         
-    def OnQuit(self, e):
+        # split path into list
+        splitPathList = path.split(os.path.sep) 
+        
+        # start from end and iterate through each path element,
+        for pathItem in reversed(splitPathList):
+            # sum the lenth of the path so far
+            pathLength += len(pathItem)
+            # if less than max
+            if pathLength <= pathLengthMax:
+                # create shorted path
+                shortenedPathList.append(pathItem)
+            else:
+                # no need to go through the loop
+                break
+        
+        # reverse the shortened path, convert to a string
+        shortenedPathList.reverse()
+        shortenedPath = os.path.sep.join(shortenedPathList)
+        # for shorter path, add ...
+        if (not shortenedPath.startswith(os.path.sep)):
+            return ".." + os.path.sep + shortenedPath 
+        else:
+            return shortenedPath
+
+    def onOpenFile(self, e):
+        """
+        Create and show the Open FileDialog
+        """
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultFile="",
+            wildcard="*.*",
+            style=wx.OPEN | wx.CHANGE_DIR
+            )
+        dlg.CentreOnParent()
+        if dlg.ShowModal() == wx.ID_OK:
+            self.inputFilePath = dlg.GetPath()
+            print "You chose the following file: " + self.inputFilePath
+            self.choosenFileText.SetLabel(self.shortenPath(self.inputFilePath,45))
+
+        dlg.Destroy()
+    
+    def onOpenFolder(self, e):
+        """
+        Create and show the Open DirDialog
+        """
+        dlg = wx.DirDialog(
+            self, message="Choose a folder",
+            style=wx.DD_DEFAULT_STYLE | wx.DD_CHANGE_DIR
+            )
+        dlg.CentreOnParent()
+        if dlg.ShowModal() == wx.ID_OK:
+            self.outputFolderPath = dlg.GetPath()
+            print "You chose the following folder: " + self.outputFolderPath
+            self.choosenFolderText.SetLabel(self.shortenPath(self.outputFolderPath,45))
+
+        dlg.Destroy()
+    
+    def onAction(self, e):
+        if(self.actionButton.GetLabel() == "Start"):
+            self.statusGauge.SetValue(20)
+            self.actionButton.SetLabel("Stop")
+        elif (self.actionButton.GetLabel() == "Stop"):
+            self.actionButton.SetLabel("Start")
+            self.statusGauge.SetValue(0)
+
+    def onQuit(self, e):
         self.Close()
 
 if __name__ == '__main__':
