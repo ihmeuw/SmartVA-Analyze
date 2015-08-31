36204c0e9da8072c5cf82c04c43c6e243f0d0be1
vitazbt <vitazbt@uw.edu>
2015-08-30 23:32:44 -0700

    [headers] Organize imports. Reformat code.

--- smartva/headers.py
+++ smartva/headers.py
@@ -1,14 +1,18 @@
-#!/opt/virtualenvs/ihme-va/bin/pythonw
-
 import csv
-import string
-import wx
-import workerthread
 import os
+import string
+
+import wx
+
+import workerthread
+
 
 # Thread class that executes processing
-class Headers():
-    """Worker Thread Class."""
+class Headers(object):
+    """
+    TODO - Write good doc string.
+    """
+
     def __init__(self, notify_window, input_file, output_dir):
         self._notify_window = notify_window
         self.inputFilePath = input_file
@@ -16,10 +20,10 @@ class Headers():
         self.want_abort = 0
 
     def run(self):
-        reader = csv.reader(open( self.inputFilePath, 'Ub'))
+        reader = csv.reader(open(self.inputFilePath, 'Ub'))
         writer = csv.writer(open(self.output_dir + os.sep + 'cleanheaders.csv', 'wb', buffering=0))
-        
-        updatestr = "Cleaning column headers\n"
+
+        updatestr = 'Cleaning column headers\n'
         wx.PostEvent(self._notify_window, workerthread.ResultEvent(updatestr))
 
         first = 1
@@ -27,13 +31,13 @@ class Headers():
         for row in reader:
             count = count + 1
             newrow = list()
-            if (first == 1):
+            if first == 1:
                 first = 2
                 for col in row:
                     newcolname = ""
                     try:
-                        lastindex = string.rindex(col, "-")
-                        newcolname = col[lastindex+1:]
+                        lastindex = string.rindex(col, '-')
+                        newcolname = col[lastindex + 1:]
                     except ValueError:
                         newcolname = col
                     newrow.append(newcolname)
@@ -42,14 +46,13 @@ class Headers():
             else:
                 writer.writerow(row)
 
-        #if count is less than 2, we have no data to process
+        # if count is less than 2, we have no data to process
         if count < 2:
-            updatestr = "No data to process, stopping\n"
+            updatestr = 'No data to process, stopping\n'
             wx.PostEvent(self._notify_window, workerthread.ResultEvent(updatestr))
             return 0
         else:
             return 1
 
-    	
     def abort(self):
         self.want_abort = 1
