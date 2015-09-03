2902558f18232e8296ecb190392ee5113f8da99d
vitazbt <vitazbt@uw.edu>
2015-09-03 14:11:12 -0700

    [loggers/application] Remove default console handler.

--- smartva/loggers/application.py
+++ smartva/loggers/application.py
@@ -1,11 +1,6 @@
 import logging
 
 logger = logging.getLogger()
-console_handler = logging.StreamHandler()
-console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(console_formatter)
-console_handler.setLevel(logging.DEBUG)
-logger.addHandler(console_handler)
 
 status_logger = logging.getLogger('status')
 status_logger.setLevel(logging.INFO)
