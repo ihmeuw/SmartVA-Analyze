37a0f85f98e804c0ae266be1a705bbb99f42070c
Brett Vitaz <vitazbt@uw.edu>
2015-09-20 17:50:26 -0700

    [vaprep] Add try block for free_text header index in processing logic.

--- smartva/vaprep.py
+++ smartva/vaprep.py
@@ -452,23 +452,27 @@ class VaPrep(object):
 
         # this just does a substitution of words in the above list (mostly misspellings, etc..)
         for question in FREE_TEXT:
-            index = headers.index(question)
-            for row in matrix:
-                answer = row[index]
-                new_answer = re.sub('[^a-z ]', '', answer.lower())
+            try:
+                index = headers.index(question)
+            except ValueError:
+                warning_logger.debug('Free text column "{}" does not exist.'.format(question))
+            else:
+                for row in matrix:
+                    answer = row[index]
+                    new_answer = re.sub('[^a-z ]', '', answer.lower())
 
-                # TODO - Fix this. It replaces partial words (e.g. bit and bite) and can cause errors.
-                # check to see if any of the keys exist in the freetext (keys can be multiple words like 'dog bite')
-                for key in WORD_SUBS.keys():
-                    if key in new_answer:
-                        # if it exists, replace it with the word(s) from the dictionary
-                        new_answer = string.replace(new_answer, key, WORD_SUBS[key])
+                    # TODO - Fix this. It replaces partial words (e.g. bit and bite) and can cause errors.
+                    # check to see if any of the keys exist in the freetext (keys can be multiple words like 'dog bite')
+                    for key in WORD_SUBS.keys():
+                        if key in new_answer:
+                            # if it exists, replace it with the word(s) from the dictionary
+                            new_answer = string.replace(new_answer, key, WORD_SUBS[key])
 
-                # now make sure we get rid of all extra whitespace
-                newanswerarray = new_answer.split(' ')
-                for i, word in enumerate(newanswerarray):
-                    newanswerarray[i] = word.strip()
-                row[index] = ' '.join(newanswerarray)
+                    # now make sure we get rid of all extra whitespace
+                    newanswerarray = new_answer.split(' ')
+                    for i, word in enumerate(newanswerarray):
+                        newanswerarray[i] = word.strip()
+                    row[index] = ' '.join(newanswerarray)
 
         # going forward, now all of the answers are lowercase, without numbers or punctuation,
         # and whitespace has been eliminated, so it's just a list of words separated by spaces
