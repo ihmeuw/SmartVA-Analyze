84e39edb655f8e3cdcc1e17143ae1a420cc9f437
vitazbt <vitazbt@uw.edu>
2015-10-27 12:10:05 -0700

    [utils] Rework and simplify shorten path logic to be more in line with standards.

--- smartva/utils/utils.py
+++ smartva/utils/utils.py
@@ -4,34 +4,11 @@ import re
 import threading
 
 
-def shorten_path(path, maxLength):
-    pathLength = 0
-    pathLengthMax = maxLength
-    shortenedPathList = []
+def shorten_path(path, max_length):
+    if len(path) <= max_length:
+        return path
 
-    # split path into list
-    splitPathList = path.split(os.path.sep)
-
-    # start from end and iterate through each path element,
-    for pathItem in reversed(splitPathList):
-        # sum the length of the path so far
-        pathLength += len(pathItem)
-        # if less than max
-        if pathLength <= pathLengthMax:
-            # create shorted path
-            shortenedPathList.append(pathItem)
-        else:
-            # no need to go through the loop
-            break
-
-    # reverse the shortened path, convert to a string
-    shortenedPathList.reverse()
-    shortenedPath = os.path.sep.join(shortenedPathList)
-    # for shorter path, add ...
-    if shortenedPath.startswith(os.path.sep) or re.match('[A-Z]:', shortenedPath):
-        return shortenedPath
-    else:
-        return '..' + os.path.sep + shortenedPath
+    return path[:int(max_length * .25)] + ' ~ ' + path[-int(max_length * .75):]
 
 
 def synchronized(fn):
