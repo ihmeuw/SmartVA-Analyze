aa223f0713739033a926e265df0d97c21999f2ae
Brett Vitaz <vitazbt@uw.edu>
2015-11-18 02:12:14 -0800

    [cause grapher] Refactor module to simplify processing and make more consistent.  - Fix bug with age group processing.  - Add and enhance comments.  - Specify input and output directories in the signature.  - Clean up code.

--- smartva/cause_grapher.py
+++ smartva/cause_grapher.py
@@ -1,84 +1,91 @@
 import csv
 import os
 import re
-from collections import defaultdict
-from collections import OrderedDict
+from collections import defaultdict, OrderedDict
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+from smartva.common_data import MALE, FEMALE, ADULT, CHILD, NEONATE
 from smartva.loggers import status_logger
 from smartva.utils import status_notifier
 
+INPUT_FILENAME_TEMPLATE = '{:s}-predictions.csv'
+OUTPUT_FILENAME_TEMPLATE = '{:s}-figure.png'
+
+
 # labels for dict
-module_labels = ('adult', 'child', 'neonate')
-age_labels = ('0-28 days', '29 days - 1 year', '1-4 years', '5-11 years', '12-19 years', '20-44 years', '45-59 years', '60+ years', 'unknown')
-gender_labels = ('male', 'female', 'unknown')
+MODULE_LABELS = (ADULT, CHILD, NEONATE)
+
+AGE_DATA = OrderedDict(
+    (
+        (60.0, '60+ years'),
+        (45.0, '45-59 years'),
+        (20.0, '20-44 years'),
+        (12.0, '12-19 years'),
+        (5.0, '5-11 years'),
+        (1.0, '1-4 years'),
+        (29 / 365.0, '29 days - 1 year'),
+        (0.0, '0-28 days')
+    )
+)
+
+GENDER_DATA = OrderedDict(
+    (
+        (MALE, 'male'),
+        (FEMALE, 'female'),
+    )
+)
 
 
 # default dict for cause of death graph
 def get_default_dict():
+    """Helper function to create a graph data default dict template.
+
+    :return: Graph data default dict template.
+    """
     default_dict = dict()
-    for gender in gender_labels:
-        default_dict[gender] = OrderedDict.fromkeys(age_labels, 0)
+    for gender in GENDER_DATA:
+        default_dict[gender] = OrderedDict.fromkeys(reversed(AGE_DATA.values()), 0)
     return default_dict
 
 
-# build ordered dict for values to be graphed. indexed by cause, the gender, then age.
-graph_data = defaultdict(get_default_dict)
+def get_age_key(age_value):
+    """Helper function to identify age group by age.
 
-
-# convert value from csv to key for dict
-def get_gender_key(gender_value):
-    gender_key = 'unknown'
-    if gender_value == '2':
-        gender_key = 'female'
-    elif gender_value == '1':
-        gender_key = 'male'
-    return gender_key
-
-
-# convert value from csv to key for dict
-def get_age_key(module_key, age_value):
-    age_key = 'unknown'
-    # TODO are age values correct for neonate and child?
-    if module_key == 'neonate' and (age_value >= 0 and age_value <= 28):
-        age_key = '0-28 days'
-    elif module_key == 'child' and (age_value > 0 and age_value < 12):
-        age_key = '29 days - 1 year'
-    elif module_key == 'adult' and (age_value >= 1 and age_value <= 4):
-        age_key = '1-4 years'
-    elif module_key == 'adult' and (age_value >= 5 and age_value <= 11):
-        age_key = '5-11 years'
-    elif module_key == 'adult' and (age_value >= 12 and age_value <= 19):
-        age_key = '12-19 years'
-    elif module_key == 'adult' and (age_value >= 20 and age_value <= 44):
-        age_key = '20-44 years'
-    elif module_key == 'adult' and (age_value >= 45 and age_value <= 59):
-        age_key = '45-59 years'
-    elif module_key == 'adult' and (age_value >= 60):
-        age_key = '60+ years'
-    return age_key
+    :param age_value: Age in years.
+    :return: String representation of age group.
+    """
+    for k, v in AGE_DATA.items():
+        if age_value >= k:
+            return v
+    return 'Unknown'
 
 
 # make and save cause graph
-def make_graph(cause_key, output_dir):
-    male_data = graph_data[cause_key]['male'].values()
-    female_data = graph_data[cause_key]['female'].values()
-    unknown_data = graph_data[cause_key]['unknown'].values()
+def make_graph(graph_data, cause_key, output_dir):
+    """Generate and save a cause graph.
+
+    :param graph_data: Graph data dict.
+    :param cause_key: Name of the cause for which to generate graph.
+    :param output_dir: Directory in which to save graph.
+    """
+    male_data = graph_data[MALE].values()
+    female_data = graph_data[FEMALE].values()
 
     graph_title = cause_key.capitalize() + ' by age and sex'
     graph_filename = re.sub('[^\w_\. ]', '-', cause_key.replace('(', '').replace(')', '')).replace(' ', '-').lower()
 
-    max_value = max(max(male_data), max(female_data), max(unknown_data))
-    xlocations = np.arange(len(age_labels))  # the x locations for the groups
+    max_value = max(max(male_data), max(female_data))
+    xlocations = np.arange(len(AGE_DATA))  # the x locations for the groups
 
     bar_width = 0.25  # the width of the bars
 
-    # interactive mode off
+    # Interactive mode off.
     plt.ioff()
     fig, ax = plt.subplots()
 
+    # Place male and female bars next to each other.
     rects1 = ax.bar(xlocations, male_data, bar_width, color='#C44440', align='center')
     rects2 = ax.bar(xlocations + bar_width, female_data, bar_width, color='#1D72AA', align='center')
 
@@ -86,78 +93,75 @@ def make_graph(cause_key, output_dir):
     ax.set_ylabel('Number of VAs')
     ax.yaxis.grid()
 
-    ax.set_xticklabels(age_labels, rotation=90)
+    ax.set_xticklabels(reversed(AGE_DATA.values()), rotation=90)
     ax.set_xticks(xlocations + bar_width / 2)
 
-    # push legend outside of the plot
-    ax.legend((rects1[0], rects2[0]), gender_labels, loc='upper center', bbox_to_anchor=(0.5, -0.375), ncol=2)
+    # Push legend outside of the plot.
+    ax.legend((rects1[0], rects2[0]), GENDER_DATA.values(), loc='upper center', bbox_to_anchor=(0.5, -0.375), ncol=2)
 
-    # add whitespace at top of bar
+    # Add whitespace at top of bar.
     ax.set_ylim(top=max_value + .5)
 
-    # add whitespace before first bar and after last
+    # Add whitespace before first bar and after last.
     plt.xlim([min(xlocations) - .5, max(xlocations) + 1.0])
 
-    # add some spacing for rotated xlabels
+    # Add some spacing for rotated xlabels.
     plt.subplots_adjust(bottom=0.35)
 
-    # clean up filenames
-    plt.savefig(output_dir + os.sep + graph_filename + '-figure.png', dpi=150)
+    # Save graph figure.
+    plt.savefig(os.path.join(output_dir, OUTPUT_FILENAME_TEMPLATE.format(graph_filename)), dpi=150)
 
-    # clear the current figure
+    # Clear the current figure.
     plt.clf()
     plt.close()
 
 
 class CauseGrapher(object):
-    def __init__(self, input_file, output_dir):
-        self.inputFilePath = input_file
+    """Generate and save a graph for each cause, and one for all causes."""
+
+    def __init__(self, input_dir, output_dir):
+        self.input_dir = input_dir
         self.output_dir = output_dir
-        self.want_abort = 0
+        self.want_abort = False
 
     def run(self):
         status_logger.info('Making cause graphs')
         status_notifier.update({'progress': 1})
 
-        module_errors = {}
-        status_notifier.update({'sub_progress': (0, len(module_labels))})
+        graph_data = defaultdict(get_default_dict)
 
-        for cnt, module_key in enumerate(module_labels):
+        status_notifier.update({'sub_progress': (0, len(MODULE_LABELS))})
+
+        for cnt, module_key in enumerate(MODULE_LABELS):
             status_notifier.update({'sub_progress': (cnt,)})
 
-            # read and process data from csv. rU gives universal newline support
-            # TODO what happens if you don't have a module
             try:
-                csv_file = csv.DictReader(
-                    open(self.inputFilePath.replace('$module-predictions.csv', module_key + '-predictions.csv'), 'rU'))
+                with open(os.path.join(self.input_dir, INPUT_FILENAME_TEMPLATE.format(module_key)), 'rb') as f:
+                    reader = csv.DictReader(f)
 
-                module_errors[module_key] = 0
+                    for row in reader:
+                        if self.want_abort:
+                            return
 
-                for row in csv_file:
-                    if self.want_abort == 1:
-                        return
+                        age_key = get_age_key(float(row['age']))
+                        sex_key = int(row['sex'])
 
-                    age_key = get_age_key(module_key, float(row['age']))
-                    gender_key = get_gender_key(row['sex'])
-                    cause_key = row['cause34']
-
-                    graph_data[cause_key][gender_key][age_key] += 1
-                    graph_data['All'][gender_key][age_key] += 1
+                        graph_data[row['cause34']][sex_key][age_key] += 1
+                        graph_data['All'][sex_key][age_key] += 1
 
             except IOError:
-                # if the file isn't there, there was no data or an error, so just skip it
-                # print module_key+'-predictions.csv not found'
-                module_errors[module_key] = 1
+                # The file isn't there, there was no data or an error, so just skip it.
+                continue
 
-        # make cause of death graphs
+        # Make cause of death graphs.
         status_notifier.update({'sub_progress': (0, len(graph_data))})
 
-        for cnt, cause_key in enumerate(graph_data.keys()):
+        for cnt, (cause_key, data) in enumerate(graph_data.items()):
             status_notifier.update({'sub_progress': (cnt,)})
 
-            make_graph(cause_key, self.output_dir)
+            make_graph(data, cause_key, self.output_dir)
 
         status_notifier.update({'sub_progress': None})
 
     def abort(self):
-        self.want_abort = 1
+        self.want_abort = True
