3154cdc5d5d186c706b70c8a4022199a06b85a78
Brett Vitaz <vitazbt@uw.edu>
2016-04-28 14:44:43 -0700

    [tariff] Add functional tests for adult, child, and neonate tariff modules.

---
+++ test/tariff_prep/test_child_tariff_prep.py
@@ -0,0 +1,59 @@
+import csv
+import os
+import shutil
+
+import pytest
+
+from smartva.child_tariff import ChildTariff
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.fixture
+def input_file(tmpdir):
+    dest = tmpdir.mkdir('intermediate-files').join('child-symptom.csv')
+    shutil.copy(local_file('child-symptom.csv'), dest.strpath)
+    return dest
+
+
+@pytest.fixture
+def prep(tmpdir):
+    return ChildTariff(
+        working_dir_path=tmpdir.strpath,
+        short_form=True,
+        options={'hce': True, 'free_text': True, 'hiv': True, 'malaria': True},
+        country='USA'
+    )
+
+
+def local_file(filename):
+    return os.path.join(path, filename)
+
+
+def get_expected_results(file_):
+    with open(file_, 'rb') as f:
+        r = csv.DictReader(f)
+        return [row for row in r]
+
+
+def validate_matrix(actual, expected):
+    for a in actual:
+        e = expected.next()
+        for var in e:
+            assert a[var] == e[var], "SID: '{}' does not produce expected result".format(a['sid'])
+
+
+def validate_predictions(file_):
+    assert file_.check()
+    with file_.open('rb') as f:
+        r = csv.DictReader(f)
+        actual_results = [row for row in r]
+
+    validate_matrix(iter(actual_results), iter(get_expected_results(local_file('child-predictions.csv'))))
+
+
+@pytest.mark.skipif(os.environ.get('SKIP_FUNCTIONAL', 'false').lower() == 'true', reason='Skipping functional test.')
+def test_tariff_prep(prep, input_file, tmpdir):
+    prep.run()
+
+    validate_predictions(tmpdir.join('child-predictions.csv'))
