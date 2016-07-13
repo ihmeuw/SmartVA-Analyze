575471edde83f1189746eab593c76fd60dbe7981
Brett Vitaz <vitazbt@uw.edu>
2016-07-13 10:28:34 -0700

    [format headers] fix and simplify test. no longer instantiates `WorkerThread`, only tests method in question

--- test/test_workerthread.py
+++ test/test_workerthread.py
@@ -1,19 +1,8 @@
-import pytest
+from smartva.workerthread import WorkerThread
 
-from smartva import workerthread
 
-@pytest.fixture
-def worker_thread(tmpdir):
-    out_dname = str(tmpdir.mkdir('out'))
-    def completion_callback(a,b):
-        pass
+def test_format_headers():
+    headers = ['header', 'foo-header', 'bar-baz-header', 'foo:header', 'bar:baz:header']
 
-    return workerthread.WorkerThread('in', out_dname, {}, '', completion_callback)
-    
-class TestWorkerThread(object):
-    def test_format_headers(self, worker_thread):
-        headers = ['header', 'foo-header', 'bar-baz-header', 'foo:header', 'bar:baz:header']
-        
-        for h in headers:
-            assert worker_thread._format_header(h) == 'header', '{} is not a clean header'.format(h)
-
+    for h in headers:
+        assert WorkerThread._format_header(h) == 'header', '{} is not a clean header'.format(h)
