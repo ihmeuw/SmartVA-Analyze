c09cda8505302b519eca2882ab2d1e75e442a833
Brett Vitaz <vitazbt@uw.edu>
2016-07-20 17:08:33 -0700

    [rule-engine] add road traffic rule. SVA-303

---
+++ smartva/rules/road_traffic_child.py
@@ -0,0 +1,15 @@
+# https://stash.ihme.washington.edu/projects/VA/repos/business_rules/browse/doc/md/rti.md
+from smartva.data.constants import *
+from smartva.utils.utils import value_from_row, int_or_float
+
+CAUSE_ID = 19
+
+
+def logic_rule(row):
+    value_of = value_from_row(row, int_or_float)
+
+    road_traffic = value_of(Child.ROAD_TRAFFIC) == YES
+
+    terminal = value_of(Child.INJURY_DAYS) < 30
+
+    return road_traffic and terminal
