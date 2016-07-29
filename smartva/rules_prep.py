d16194114920d50bb67937cf17611909e98bec8e
Brett Vitaz <vitazbt@uw.edu>
2016-07-28 18:27:03 -0700

    [rule-engine] fix for import issue that occurs with pyinstaller

--- smartva/rules_prep.py
+++ smartva/rules_prep.py
@@ -4,7 +4,35 @@ import smartva.data.common_data as common_data
 from smartva.data_prep import DataPrep
 from smartva.loggers import status_logger, warning_logger
 from smartva.utils import status_notifier
-from smartva.rules import *
+from smartva.rules import (
+    anemia,
+    bite_adult,
+    bite_child,
+    cancer_child,
+    drowning_adult,
+    drowning_child,
+    falls_adult,
+    falls_child,
+    fires_adult,
+    fires_child,
+    hemorrhage,
+    homicide_adult,
+    homicide_child,
+    hypertensive,
+    malnutrition,
+    measles,
+    other_injury_adult,
+    other_injury_child,
+    other_pregnancy,
+    poisoning_adult,
+    poisoning_child,
+    road_traffic_adult,
+    road_traffic_child,
+    sepsis,
+    stillbirth,
+    suicide,
+    tetanus_neonate,
+)
 
 INPUT_FILENAME_TEMPLATE = '{:s}-presymptom.csv'
 OUTPUT_FILENAME_TEMPLATE = '{:s}-logic-rules.csv'
