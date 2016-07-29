13bcaeb94359b53ffb3a0410d9e7f3b24605310f
Brett Vitaz <vitazbt@uw.edu>
2016-07-28 19:06:53 -0700

    [build-agent] add PYTHONPATH to test runner

--- pkg/build-agent/run_tests.sh
+++ pkg/build-agent/run_tests.sh
@@ -2,11 +2,10 @@ TEST_FILE=test-results.xml
 
 cd ~/build-agent/smartva
 rm ../$TEST_FILE | true
-env/Scripts/py.test --junitxml=../$TEST_FILE test
+PYTHONPATH=. env/Scripts/py.test --junitxml=../$TEST_FILE test
 
 if [[ -f ../$TEST_FILE ]]; then
   exit 0
 fi
 
 exit 1
-
