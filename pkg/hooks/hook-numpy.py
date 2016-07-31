5579acb7666187c873f1f76eee01be5680203a3f
Brett Vitaz <vitazbt@uw.edu>
2016-07-31 14:44:02 -0700

    [build-agent] fix numpy hook for Python 2

--- pkg/hooks/hook-numpy.py
+++ pkg/hooks/hook-numpy.py
@@ -17,6 +17,5 @@ try:
         logger = logging.getLogger(__name__)
         logger.info("MKL installed as part of numpy, importing that!")
         binaries = [(libdir + "/" + l, '') for l in mkllib]
-except FileNotFoundError:
+except OSError:
     pass
-
