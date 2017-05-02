a1b58de0e66a10448047641b56972d7f17adc47b
Jonathan Joseph <josephj7@uw.edu>
2017-05-02 13:44:23 -0700

    BUG: fix more errors in adult stroke rule, add test to ensure it runs.

--- smartva/rules/stroke.py
+++ smartva/rules/stroke.py
@@ -7,24 +7,24 @@ CAUSE_ID = 44
 def logic_rule(row):
     value_of = value_from_row(row, int_or_float)
 
-    stroke = value_of(ADULT.STROKE) == YES
-    paralysis_one_side = (value_of(ADULT.PARALYSIS_RIGHT) == YES or
-                          value_of(ADULT.PARALYSIS_LEFT) == YES)
+    stroke = value_of(Adult.STROKE) == YES
+    paralysis_one_side = (value_of(Adult.PARALYSIS_RIGHT) == YES or
+                          value_of(Adult.PARALYSIS_LEFT) == YES)
 
     any_paralysis_or_stroke = any([
         stroke,
         paralysis_one_side,
-        value_of(ADULT.PARALYSIS_LOWER) == YES,
-        value_of(ADULT.PARALYSIS_UPPER) == YES,
-        value_of(ADULT.PARALYSIS_ONE_LEG) == YES,
-        value_of(ADULT.PARALYSIS_ONE_ARM) == YES,
-        value_of(ADULT.PARALYSIS_ONE_WHOLE) == YES,
-        value_of(ADULT.PARALYSIS_ONE_OTHER) == YES,
+        value_of(Adult.PARALYSIS_LOWER) == YES,
+        value_of(Adult.PARALYSIS_UPPER) == YES,
+        value_of(Adult.PARALYSIS_ONE_LEG) == YES,
+        value_of(Adult.PARALYSIS_ONE_ARM) == YES,
+        value_of(Adult.PARALYSIS_WHOLE) == YES,
+        value_of(Adult.PARALYSIS_OTHER) == YES,
     ])
 
-    diabetes = value_of(ADULT.DIABETES) == YES
+    diabetes = value_of(Adult.DIABETES) == YES
 
-    pneumonia = value_of(ADULT.FREE_TEXT_PNEUMONIA) == YES
+    pneumonia = value_of(Adult.FREE_TEXT_PNEUMONIA) == YES
 
     return ((stroke and paralysis_one_side) or
             (diabetes and any_paralysis_or_stroke) or
