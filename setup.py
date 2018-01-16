4ec9f76b6c6e47a1610de9d202992f8819af975a
Jonathan Joseph <josephj7@uw.edu>
2018-01-16 15:34:00 -0800

    BLD: Add setup file to make smartva installable

---
+++ setup.py
@@ -0,0 +1,24 @@
+import re
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt', 'rt') as f:
+    REQUIREMENTS = f.readlines()
+
+with open('smartva/__init__.py', 'rt') as f:
+    VERSION = re.search("(?<=^version = ').+(?='$)", f.read(), re.M).group()
+
+
+setup(
+    name='smartva',
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    entry_points={
+        'console_scripts': [
+            'smartva=smartva.va_cli:main',
+        ]
+    },
+    python_requires='~=2.7',
+)
