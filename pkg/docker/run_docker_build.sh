a7f117f3fc33dbd21c9b901ba440f4013d7228d0
Jonathan Joseph <josephj7@uw.edu>
2018-01-29 11:12:13 -0800

    BLD: Separate python package requirements
    * requirements.txt contains packages needed for running the app
    * requirements-dev.txt contains packages needed for the build
    * requirements-win.txt are windows-specific requirements

--- pkg/docker/run_docker_build.sh
+++ pkg/docker/run_docker_build.sh
@@ -6,7 +6,10 @@ cp -r smartva/pkg/build-agent/. build-agent/
 cp smartva/pkg/docker/run_tests_and_build_in_wine.patch .
 git apply run_tests_and_build_in_wine.patch
 
-wine python -m pip install -U -r smartva/requirements.txt
+wine python -m pip install -U \
+      -r smartva/requirements.txt \
+      -r smartva/requirements-dev.txt \
+      -r smartva/requirements-win.txt
 
 cd build-agent
 ./run_tests.sh
