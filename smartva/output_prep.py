7111e83e19849b127e07d946c966d1e65079f6dd
Jonathan Joseph <josephj7@uw.edu>
2018-02-06 11:22:34 -0800

    ENH: Report age with units in individual output file

--- smartva/output_prep.py
+++ smartva/output_prep.py
@@ -143,7 +143,7 @@ class OutputPrep(DataPrep):
                                 pred_row.get('cause34'),
                                 pred_row.get('cause'),
                                 ICDS[module].get(pred_row.get('cause34')),
-                                pred_row.get('age'),
+                                self.add_age_unit(pred_row.get('age')),
                                 pred_row.get('sex'),
                                 self.make_date(*birth_date),
                                 self.make_date(*death_date),
@@ -163,6 +163,24 @@ class OutputPrep(DataPrep):
             return '{:d}-{:02d}-{:02d}'.format(*map(int, (year, month, day)))
         else:
             return ''
+
+    @staticmethod
+    def add_age_unit(age):
+        try:
+            age = float(age)
+        except (TypeError, ValueError):
+            return
+        else:
+            one_month = 30. / 365
+            if age >= 1:
+                age_scaled, unit = age, 'year'
+            elif one_month <= age < 1:
+                age_scaled, unit = age * 12, 'month'
+            elif 0 <= age < one_month:
+                age_scaled, unit = age * 365, 'day'
+
+        plural = 's' if age_scaled != 1 else ''
+        return '{} {}{}'.format(int(age_scaled), unit, plural)
 
     def _concatenate_likelihoods_files(self):
         likelihoods_file = os.path.join(self.working_dir_path, FOLDER1,
