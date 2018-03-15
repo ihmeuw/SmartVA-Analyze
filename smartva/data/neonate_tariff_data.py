4d3af5778818d6c54ef393ed0be009dd5215dfe7
Jonathan Joseph <josephj7@uw.edu>
2018-03-15 10:59:08 -0700

    ENH: Rename child and neonate causes.
    The new output format creates a single list of all ages. Causes
    that appear on both multiple cause lists should have distinct
    names to facility easy filtering in post-analysis.

--- smartva/data/neonate_tariff_data.py
+++ smartva/data/neonate_tariff_data.py
@@ -181,8 +181,8 @@ SHORT_FORM_DROP_LIST = [
 CAUSES = {
     1: 'Birth asphyxia',
     2: 'Congenital malformation',
-    3: 'Meningitis/Sepsis',
-    4: 'Pneumonia',
+    3: 'Neonatal Meningitis/Sepsis',
+    4: 'Neonatal Pneumonia',
     5: 'Preterm Delivery',
     6: 'Stillbirth',
 }
