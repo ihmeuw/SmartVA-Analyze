747c0a88ce604bd8129e7f11ebd76addb5ca2e3e
Jonathan Joseph <josephj7@uw.edu>
2018-03-15 14:00:27 -0700

    ENH: Change cause graph age groups
    The new age groups are 10 year bins up to 80+ and match the
    tabulation provided in the new output.

--- test/test_grapher.py
+++ test/test_grapher.py
@@ -1,5 +1,7 @@
 from collections import defaultdict, OrderedDict
 
+import pytest
+
 from smartva import cause_grapher
 from smartva.data.common_data import MALE, FEMALE
 
@@ -33,47 +35,49 @@ def test_get_default_dict_inc():
     assert d == valid_d
 
 
-def test_get_age_key_0_28():
-    assert cause_grapher.get_age_key(0) == '0-28 days'
-    assert cause_grapher.get_age_key(28. / 365) == '0-28 days'
-
-
-def test_get_age_key_29_1():
-    assert cause_grapher.get_age_key(29. / 365) == '29 days - 1 year'
-    assert cause_grapher.get_age_key(364. / 365) == '29 days - 1 year'
-
-
-def test_get_age_key_1_4():
-    assert cause_grapher.get_age_key(1) == '1-4 years'
-    assert cause_grapher.get_age_key(4.9) == '1-4 years'
-
-
-def test_get_age_key_5_11():
-    assert cause_grapher.get_age_key(5) == '5-11 years'
-    assert cause_grapher.get_age_key(11.9) == '5-11 years'
-
-
 def test_get_age_key_12_19():
     assert cause_grapher.get_age_key(12) == '12-19 years'
     assert cause_grapher.get_age_key(19.9) == '12-19 years'
 
 
-def test_get_age_key_20_44():
-    assert cause_grapher.get_age_key(20) == '20-44 years'
-    assert cause_grapher.get_age_key(44.9) == '20-44 years'
-
-
-def test_get_age_key_45_59():
-    assert cause_grapher.get_age_key(45) == '45-59 years'
-    assert cause_grapher.get_age_key(59.9) == '45-59 years'
-
-
-def test_get_age_key_60_plus():
-    assert cause_grapher.get_age_key(60) == '60+ years'
-    assert cause_grapher.get_age_key(100) == '60+ years'
-
-
-def test_get_age_key_unknown():
-    assert cause_grapher.get_age_key(-1) == 'Unknown'
-
-
+@pytest.mark.parametrize('age, label', [
+    (0, '0-28 days'),
+    (12. / 365, '0-28 days'),
+    (28. / 365, '0-28 days'),
+    (29. / 365, '29 days - 1 year'),
+    (77. / 365, '29 days - 1 year'),
+    (364. / 365, '29 days - 1 year'),
+    (1, '1-4 years'),
+    (3, '1-4 years'),
+    (4.99999, '1-4 years'),
+    (5, '5-11 years'),
+    (7, '5-11 years'),
+    (11.99999, '5-11 years'),
+    (12, '12-19 years'),
+    (17, '12-19 years'),
+    (19.99999, '12-19 years'),
+    (20, '20-29 years'),
+    (27, '20-29 years'),
+    (29.99999, '20-29 years'),
+    (30, '30-39 years'),
+    (37, '30-39 years'),
+    (39.99999, '30-39 years'),
+    (40, '40-49 years'),
+    (47, '40-49 years'),
+    (49.99999, '40-49 years'),
+    (50, '50-59 years'),
+    (57, '50-59 years'),
+    (59.99999, '50-59 years'),
+    (60, '60-69 years'),
+    (67, '60-69 years'),
+    (69.99999, '60-69 years'),
+    (70, '70-79 years'),
+    (77, '70-79 years'),
+    (79.99999, '70-79 years'),
+    (80, '80+ years'),
+    (87, '80+ years'),
+    (125, '80+ years'),
+    (-1, 'Unknown'),
+])
+def test_get_age_key(age, label):
+    assert cause_grapher.get_age_key(age) == label
