cb7f936d522ef2ac122f8d3fa9e675458ea656a8
Jonathan Joseph <josephj7@uw.edu>
2018-04-16 15:55:13 -0700

    BLD: Run data checks if CSVs change

--- pkg/build-agent/run_tests.sh
+++ pkg/build-agent/run_tests.sh
@@ -10,7 +10,15 @@ if [[ -f $TEST_FILE ]]; then
     rm $TEST_FILE
 fi
 
-$WINE python -m pytest --junitxml=$TEST_FILE test
+CURRENT_TAG=$(git log --tags --simplify-by-decoration --pretty="%d" | grep -oP "(?<=\().+(?=\))" | head -1)
+DATA_CHANGED=$(git diff --name-only $CURRENT_TAG..HEAD | grep "\.csv" | wc -l)
+if [[ $DATA_CHANGED -gt 0 ]]; then
+    PYTEST_FLAGS="--data-checks"
+else
+    PYTEST_FLAGS=""
+fi
+
+$WINE python -m pytest --junitxml=$TEST_FILE $PYTEST_FLAGS test
 
 if [[ -f $TEST_FILE ]]; then
   exit 0
