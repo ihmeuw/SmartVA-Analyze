b61d83191abfc04827b55efa88afce785023e7c8
Riley Hazard <riley.hazard@unimelb.edu.au>
2018-04-23 12:10:07 -0400

    First commit in WHO map

--- test/mapping/who2016_adult_mapping.py
+++ test/mapping/who2016_adult_mapping.py
@@ -3,5 +3,10 @@ MAPPING = [
         'sid': 'Fever',
         'symptom': 's16',
         'Id10147': 'yes'
+    },
+    {
+        'sid': 'Blood in vomit',
+        'symptom': 's74',
+        'Id10191': 'yes'
     }
 ]
