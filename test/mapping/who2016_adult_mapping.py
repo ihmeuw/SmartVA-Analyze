49a9d77adde3a070ed9f19efe60d08cdc48661ca
Riley Hazard <riley.hazard@unimelb.edu.au>
2018-04-25 13:53:34 -0400

    Add in batch of yes/no maps and WHO consent header

--- test/mapping/who2016_adult_mapping.py
+++ test/mapping/who2016_adult_mapping.py
@@ -1,8 +1,183 @@
 MAPPING = [
     {
+        'sid': 'History of asthma',
+        'symptom': 's1',
+        'Id10135': 'yes',
+    },
+    # {
+    #     'sid': 'History of arthritis',
+    #     'symptom': 's2',
+    #     'adult_1_1b': 1
+    # },
+    {
+        'sid': 'History of cancer',
+        'symptom': 's3',
+        'Id10137': 'yes',
+    },
+    {
+        'sid': 'History of COPD',
+        'symptom': 's4',
+        'Id10138': 'yes',
+    },
+    {
+        'sid': 'History of dementia',
+        'symptom': 's5',
+        'Id10139': 'yes',
+    },
+    {
+        'sid': 'History of depression',
+        'symptom': 's6',
+        'Id10140': 'yes',
+    },
+    {
+        'sid': 'History of diabetes',
+        'symptom': 's7',
+        'Id10134': 'yes',
+    },
+    {
+        'sid': 'History of epilepsy',
+        'symptom': 's8',
+        'Id10136': 'yes',
+    },
+    {
+        'sid': 'History of heart disease',
+        'symptom': 's9',
+        'Id10133': 'yes',
+    },
+    {
+        'sid': 'History of hypertension',
+        'symptom': 's10',
+        'Id10132': 'yes',
+    },
+    # {
+    #     'sid': 'History of obesity',
+    #     'symptom': 's11',
+    #     'adult_1_1k': 'yes',
+    # },
+    {
+        'sid': 'History of stroke',
+        'symptom': 's12',
+        'Id10141': 'yes',
+    },
+    {
+        'sid': 'History of tuberculosis',
+        'symptom': 's13',
+        'Id10125': 'yes',
+    },
+    {
+        'sid': 'History of AIDS',
+        'symptom': 's14',
+        'Id10127': 'yes',
+    },
+    {
         'sid': 'Fever',
         'symptom': 's16',
         'Id10147': 'yes'
+    },
+    # {
+    #      'sid': 'Fever with sweating',
+    #      'symptom': 's20',
+    #      'adult_2_6': 'yes',
+    # },
+    {
+        'sid': 'Rash',
+        'symptom': 's21',
+        'Id10233': 'yes',
+    },
+    {
+        'sid': 'Sores',
+        'symptom': 's27',
+        'Id10228': 'yes',
+    },
+    {
+        'sid': 'Sores ooze pus',
+        'symptom': 's28',
+        'Id10229': 'yes',
+    },
+    # {
+    #     'sid': 'Itching',
+    #     'symptom': 's29',
+    #     'adult_2_12': 'yes',
+    # },
+    {
+        'sid': 'Foot ulcer',
+        'symptom': 's30',
+        'Id10230': 'yes',
+    },
+    {
+        'sid': 'Foot ulcer ooze pus',
+        'symptom': 's31',
+        'Id10231': 'yes',
+    },
+    # {
+    #     'sid': 'Pins and needles in feet',
+    #     'symptom': 's33',
+    #     'adult_2_16': 'yes',
+    # },
+    # {
+    #     'sid': 'Blue lips',
+    #     'symptom': 's34',
+    #     'adult_2_17': 'yes',
+    # },
+    {
+        'sid': 'Lost weight',
+        'symptom': 's35',
+        'Id10243': 'yes',
+    },
+    {
+        'sid': 'Pale',
+        'symptom': 's37',
+        'Id10268': 'yes',
+    },
+    {
+        'sid': 'Yellow eyes',
+        'symptom': 's38',
+        'Id10265': 'yes',
+    },
+    {
+        'sid': 'Puffy face',
+        'symptom': 's42',
+        'Id10247': 'yes',
+    },
+    {
+        'sid': 'Puffy body',
+        'symptom': 's44',
+        'Id10252': 'yes',
+    },
+    {
+        'sid': 'Lump in neck',
+        'symptom': 's46',
+        'Id10255': 'yes',
+    },
+    {
+        'sid': 'Lump in armpit',
+        'symptom': 's47',
+        'Id10256': 'yes',
+    },
+    {
+        'sid': 'Lump in groin',
+        'symptom': 's48',
+        'Id10257': 'yes',
+    },
+    {
+        'sid': 'Cough',
+        'symptom': 's49',
+        'Id10153': 'yes',
+    },
+    {
+        'sid': 'Cough produce sputum',
+        'symptom': 's51',
+        'Id10155': 'yes',
+    },
+    {
+        'sid': 'Cough blood',
+        'symptom': 's52',
+        'Id10157': 'yes',
+    },
+    {
+        'sid': 'Difficulty breathing',
+        'symptom': 's53',
+        'Id10159': 'yes',
     },
     {
         'sid': 'Blood in vomit',
