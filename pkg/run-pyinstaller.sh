783ce57c41390f6e1b2fefa03945194a3ca89d6b
Naomi Alterman <naomila@uw.edu>
2025-03-07 10:58:15 -0800

    work

--- pkg/run-pyinstaller.sh
+++ pkg/run-pyinstaller.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-pyinstaller --windowed --onefile --clean app.py
+pyinstaller \
+    -p src/ \
+    --add-data src/smartva/res:./res \
+    --add-data src/smartva/data:./data \
+    --icon pkg/icon.png \
+    --windowed --onefile --clean \
+    --name SmartVA-Analyze \
+    app.py
